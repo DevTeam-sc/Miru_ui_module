0e054512fc28f3e6d824c597e5e672db98204dbd
Devteam.sc <support@devteam.sc>
2026-02-05 10:39:19 +0700

    Cleanup: Remove dev tools, documentation, and windows binaries from repo

--- miru.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Miru CLI Wrapper for Windows
-rem Enables "miru <command>" directly from PC terminal
-rem Wraps: adb shell su -c '/data/local/tmp/miru <command>'
-
-set "CMD=%*"
-if "%CMD%"=="" set "CMD=status"
-
-adb shell "su -c '/data/local/tmp/miru %CMD%'"
--- tools/python/README.txt
+++
@@ -1,4 +0,0 @@
-Put 'Python Embeddable Package' contents here.
-1. Download "Windows embeddable package (64-bit)" from python.org
-2. Extract all files (python.exe, python3x.dll, etc.) into this folder.
-3. Now Miru can run without installing Python on the PC!
